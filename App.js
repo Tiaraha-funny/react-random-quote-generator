cbd400b7fa7a5d27ccaf1c877e85ef4251d7bbb0
Peta Jeannie <peta.jea@onja.org>
2020-10-29 16:27:41 -0600

    fetch the random quote and create a new handle button click to random the other random

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+const QUOTE = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+
+function App() {
+  
+  const [quotes, setQuotes] = useState([]);
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    async function fetchQuotes() {
+      const res = await fetch(QUOTE);
+      const data = await res.json();
+      console.log(data);
+      setQuotes(data.quote);
+    }
+  
+    fetchQuotes();
+  }, []);
+
+  function handleRandom() {
+    console.log("Random the quotes");
+    const randomNum = Math.floor(Math.random() * data.length);
+    const randomQuote = data[randomNum];
+    setQuotes({data: randomQuote})
+  }
+  
+  return (
+    <div>
+      <h2>{quotes.quoteText}</h2>
+      <button onClick={handleRandom}>{quotes.quoteAuthor}</button>
+    </div>
+  );
+}
+export default App;
